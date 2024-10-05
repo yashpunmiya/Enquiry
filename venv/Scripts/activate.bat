f765c651c73d741b7fd88f43d59451dbe3c4ca09
Vinish Rexson <vinishrexson2005@gmail.com>
2024-10-05 14:21:01 +0530

    App created

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\vinis\OneDrive\Desktop\New\venv
+set VIRTUAL_ENV=C:\Users\vinis\OneDrive\Desktop\New\base\Enquiry\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
